8a817c613d89579d3b4541e55fbb27a7e5387779
KoofNG <israelakpan296@gmail.com>
2019-09-06 10:26:16 +0100

    Initial Spring Setup

---
+++ src/main/java/bls/BlsApplication.java
@@ -0,0 +1,13 @@
+package bls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlsApplication.class, args);
+    }
+
+}
